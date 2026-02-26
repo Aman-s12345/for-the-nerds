fa8f988980f4b091fd70617cfbbf135910019c8c
aman.singh <aman.singh@myfindoc.com>
2026-02-26 05:33:59 +0530

    feat : update tamplate

---
+++ arrays/arrays.go
@@ -0,0 +1,12 @@
+package arrays
+
+import (
+	"fmt"
+)
+
+type Array struct {
+	majorityElementBruteForce (num []int) int
+	majorityElementHashMap (num []int) int
+	majorityElementSorting (num []int) int
+	majorityElementBoyerMoore (num []int) int
+}
