b5702129066a225a90f26591d9b4ec8b5c915ec8
aman.singh <aman.singh@myfindoc.com>
2026-05-29 12:30:23 +0530

    Solve : formated

--- apna-college-dsa/linked_list/ll.go
+++ apna-college-dsa/linked_list/ll.go
@@ -7,21 +7,18 @@ type LinkedList interface {
 	// Reverse the List
 	ReverseList(head *ListNode) *ListNode
 	// Detect Cycle
-	 DetectCycle(head *ListNode) *ListNode 
-	 // Has Cycle 
-	 HasCycle(head *ListNode) bool
-	 // Merge Two Sorted Lists
-	 MergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode
-	 MergeTwoListsInPlace(list1 *ListNode, list2 *ListNode) *ListNode
-	 // Flatten a Multilevel Doubly Linked List
-	 Flatten(root *Node) *Node
-	 // Check Palindrome
-	 IsPalindrome(head *ListNode) bool
-	 // Deep copy of a linked list with random pointer
-	 CopyRandomList(head *NodeRandom) *NodeRandom
-	 
-
-	 
+	DetectCycle(head *ListNode) *ListNode
+	// Has Cycle
+	HasCycle(head *ListNode) bool
+	// Merge Two Sorted Lists
+	MergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode
+	MergeTwoListsInPlace(list1 *ListNode, list2 *ListNode) *ListNode
+	// Flatten a Multilevel Doubly Linked List
+	Flatten(root *Node) *Node
+	// Check Palindrome
+	IsPalindrome(head *ListNode) bool
+	// Deep copy of a linked list with random pointer
+	CopyRandomList(head *NodeRandom) *NodeRandom
 }
 
 type linkedList struct{}
